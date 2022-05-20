c501412165552bc6eb3a0c1bec4559684a8dccb2
Al3ksiy <karelin.oleksiy@yandex.ru>
2022-05-20 23:52:12 +0700

    CabinetController fix

--- src/main/java/com/pizzeriaweb/bokoffpizza/controller/CabinetController.java
+++ src/main/java/com/pizzeriaweb/bokoffpizza/controller/CabinetController.java
@@ -7,6 +7,7 @@ import com.pizzeriaweb.bokoffpizza.service.OrderService;
 import com.pizzeriaweb.bokoffpizza.service.RegisteredUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/cabinet")
+@CrossOrigin( origins = "*", maxAge = 3500)
 public class CabinetController {
 
     @Autowired
